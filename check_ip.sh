19e85cc3706cfc48c9ca46fa75b99096121eec47
ismail taibi <61507239+ismadevjs@users.noreply.github.com>
2024-09-19 00:28:41 +0100

    xvx

--- check_ip.sh
+++ check_ip.sh
@@ -6,8 +6,18 @@ LOGFILE="/app/visitor_log.txt"
 # Get the current date and time
 TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
 
+# Log start of execution
+echo "$TIMESTAMP - Script started" >> $LOGFILE
+
+# Fetch the IP address of the container
+CONTAINER_IP=$(curl -s https://api.ipify.org)
+
 # Fetch the visitor data
 VISITOR_DATA=$(curl -s -o /dev/null -w "%{http_code} %{url_effective}\n" https://jidli.com:8443/)
 
-# Log the visitor data
+# Log the visitor data and IP
+echo "$TIMESTAMP - Container IP: $CONTAINER_IP" >> $LOGFILE
 echo "$TIMESTAMP - Visitor Data: $VISITOR_DATA" >> $LOGFILE
+
+# Log end of execution
+echo "$TIMESTAMP - Script finished" >> $LOGFILE
